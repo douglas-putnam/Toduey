ffe2349bea17ab9a8d130841912247c2c723d5b5
Douglas S. Putnam Jr <dputnam@primeassetfund.com>
2018-06-26 14:06:58 -0400

    Make cells toggle between checked and unchecked accessory

--- Toduey/TodoListViewController.swift
+++ Toduey/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController
 //  Toduey
 //
 //  Created by Douglas Putnam on 6/24/18.
@@ -10,14 +10,41 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mikey","Buy Eggos","Destroy Demidog"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //declare cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        //define cell
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        //Change selected cell
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        //add and remove check mark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
     }
 
 
