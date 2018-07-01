cb24496e980653a1ae9aca8597cdf43ee716fd4c
douglas-putnam <douglasputnam@gmail.com>
2018-07-01 07:24:10 -0400

    Add Item data model

---
+++ Toduey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Toduey
+//
+//  Created by Douglas Putnam on 7/1/18.
+//  Copyright © 2018 Douglas Putnam. All rights reserved.
+//
+
+import Foundation
+
+class item {
+    
+    var title : String = ""
+    var isDone : Bool = true
+    
+}
